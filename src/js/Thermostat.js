fb7d7e779cf2f84e6e161a9279c91f7ff94de38b
Enrico Graziani <mrenrich84@gmail.com>
2017-02-03 15:42:54 +0000

    Thermostat.js constants are now declared inside the function, changed setMaxTemp() to getMaxTemp()

--- src/js/Thermostat.js
+++ src/js/Thermostat.js
@@ -3,14 +3,15 @@
 function Thermostat() {
   this._degrees = 20;
   this._powerSaving = true;
+
+  this._MIN_TEMP = 10;
+  this._MAX_TEMP_SAVINGS = 25;
+  this._MAX_TEMP_FULL = 32;
+  this._DEFAULT_TEMP = 20;
+  this._MEDIUM_USAGE_LIMIT = 25;
+  this._LOW_USAGE_LIMIT = 18;
 }
 
-Thermostat.MIN_TEMP = 10;
-Thermostat.MAX_TEMP_SAVINGS = 25;
-Thermostat.MAX_TEMP_FULL = 32;
-Thermostat.DEFAULT_TEMP = 20;
-Thermostat.HIGH_USAGE_LIMIT = 25;
-Thermostat.LOW_USAGE_LIMIT = 18;
 
 // PUBLIC
 
@@ -27,23 +28,23 @@ Thermostat.prototype.switchPowerSaving = function() {
 };
 
 Thermostat.prototype.up = function(number) {
-  var newTemp = Math.min(this.getDegrees() + number, this._setMaxTemp());
+  var newTemp = Math.min(this.getDegrees() + number, this._getMaxTemp());
   this._setDegrees(newTemp);
 };
 
 Thermostat.prototype.down = function(number) {
-  var newTemp = Math.max(this.getDegrees() - number, Thermostat.MIN_TEMP);
+  var newTemp = Math.max(this.getDegrees() - number, this._MIN_TEMP);
   this._setDegrees(newTemp);
 };
 
 Thermostat.prototype.reset = function() {
-  this._setDegrees(Thermostat.DEFAULT_TEMP);
+  this._setDegrees(this._DEFAULT_TEMP);
 };
 
 Thermostat.prototype.seeEnergyUsage = function() {
-  if (this.getDegrees() < Thermostat.LOW_USAGE_LIMIT) {
+  if (this.getDegrees() < this._LOW_USAGE_LIMIT) {
     return 'low-usage';
-  } else if (this.getDegrees() < Thermostat.HIGH_USAGE_LIMIT) {
+  } else if (this.getDegrees() < this._MEDIUM_USAGE_LIMIT) {
     return 'medium-usage';
   } else {
     return 'high-usage';
@@ -56,6 +57,6 @@ Thermostat.prototype._setDegrees = function(number) {
   this._degrees = number;
 };
 
-Thermostat.prototype._setMaxTemp = function() {
-  return (this._powerSaving) ? Thermostat.MAX_TEMP_SAVINGS : Thermostat.MAX_TEMP_FULL;
+Thermostat.prototype._getMaxTemp = function() {
+  return (this._powerSaving) ? this._MAX_TEMP_SAVINGS : this._MAX_TEMP_FULL;
 };
